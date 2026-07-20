bee0cb646f9679cb58acd02d20b282d32a027912
brunopantaleao <95861562+brunopantaleao@users.noreply.github.com>
2026-07-20 14:11:24 -0300

    app.R

--- app.R
+++ app.R
@@ -607,7 +607,7 @@ theme = bs_theme(
     ),
      card(card_body(
         tags$h5("Como Citar?", style = "color:#611ce3;"),
-        tags$p("'Presenting the Favelas Dados dataset, a bird’s-eye view of Brazilian favelas and urban communities' -https://doi.org/10.31235/osf.io/97yb5_v1")
+        tags$p("Presenting the Favelas Dados dataset, a bird’s-eye view of Brazilian favelas and urban communities - https://doi.org/10.31235/osf.io/97yb5_v1")
       ))
     ),
     div(
